57a3db6f7411e48a2c62ef938989319df5aae752
Emily Amanda Bellows <emily.a.bellows@gmail.com>
2018-02-19 15:41:53 -0500

    Now printing a JSON graph

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ extern crate git2;
 extern crate serde;
 extern crate serde_json;
 
-#[macro_use] extern crate failure;
-#[macro_use] extern crate serde_derive;
+extern crate failure;
 
 use failure::Error;
-use git2::{Repository, Odb, OdbObject, ObjectType, Commit, Oid};
+use git2::{Commit, ObjectType, Odb, Oid, Repository};
+use serde::ser::{Serialize, SerializeMap, SerializeStruct, Serializer};
+use std::collections::BTreeMap;
 
 fn list_oids(odb: &Odb) -> Result<Vec<Oid>, Error> {
     let mut oids = vec![];
@@ -35,18 +36,108 @@ fn commits_only<'a>(repo: &'a Repository, odb: &Odb) -> Result<Vec<Commit<'a>>, 
     Ok(commits)
 }
 
+#[derive(Debug)]
+struct Node {
+    parents: Vec<Oid>,
+    children: Vec<Oid>,
+}
+
+fn oid_to_string(oid: &Oid) -> String {
+    format!("{}", oid)
+}
+
+fn oids_to_strings(oids: &[Oid]) -> Vec<String> {
+    oids.iter().map(oid_to_string).collect()
+}
+
+impl Node {
+    fn new() -> Node {
+        Node {
+            parents: vec![],
+            children: vec![],
+        }
+    }
+
+    fn parent_strings(&self) -> Vec<String> {
+        oids_to_strings(&self.parents)
+    }
+
+    fn children_strings(&self) -> Vec<String> {
+        oids_to_strings(&self.children)
+    }
+}
+
+impl Serialize for Node {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Node", 2)?;
+        state.serialize_field("parents", &self.parent_strings())?;
+        state.serialize_field("children", &self.children_strings())?;
+        state.end()
+    }
+}
+
+#[derive(Debug)]
+struct Graph {
+    graph: BTreeMap<Oid, Node>,
+}
+
+impl Serialize for Graph {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(self.graph.len()))?;
+        for (k, v) in &self.graph {
+            map.serialize_entry(&oid_to_string(&k), &v)?;
+        }
+        map.end()
+    }
+}
+
+impl Graph {
+    fn new() -> Graph {
+        Graph {
+            graph: BTreeMap::new(),
+        }
+    }
+
+    fn add(&mut self, commit: &Commit) -> Result<(), Error> {
+        let parent_ids: Vec<Oid> = commit.parent_ids().collect();
+
+        if let Some(ref mut node) = self.graph.get_mut(&commit.id()) {
+            node.parents = parent_ids;
+            return Ok(());
+        }
+
+        let mut node = Node::new();
+        for parent_id in &parent_ids {
+            if let Some(ref mut node) = self.graph.get_mut(parent_id) {
+                node.children.push(commit.id());
+            }
+        }
+        node.parents = parent_ids;
+        self.graph.insert(commit.id(), node);
+        Ok(())
+    }
+}
+
 fn calculate_descendents() -> Result<(), Error> {
     let repo = Repository::open(".")?;
     let odb = repo.odb()?;
+    let mut graph = Graph::new();
     for commit in commits_only(&repo, &odb)? {
-        println!("{} {}", commit.id(), commit.message().unwrap_or("").trim());
+        graph.add(&commit)?;
     }
+    println!("{}", serde_json::to_string_pretty(&graph)?);
     Ok(())
 }
 
 fn main() {
     match calculate_descendents() {
-        Ok(()) => {},
-        Err(err) => eprintln!("Error: {}", err)
+        Ok(()) => {}
+        Err(err) => eprintln!("Error: {}", err),
     }
 }
