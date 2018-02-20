fbbdb3539ec71c66cc8b309fb81761f89d062068
Emily Amanda Bellows <emily.a.bellows@gmail.com>
2018-02-20 14:54:48 -0500

    Improved performance of graph construction by minimizing expensive find_commit calls

--- src/calculate.rs
+++ src/calculate.rs
@@ -5,8 +5,8 @@
 // except according to those terms.
 
 use failure::Error;
-use git2::{Commit, Repository};
-use std::collections::HashSet;
+use git2::{Commit, Oid, Repository};
+use std::collections::HashMap;
 use graph::Graph;
 
 pub fn root_commits_by_refs<'a>(repo: &'a Repository) -> Result<Vec<Commit<'a>>, Error> {
@@ -22,30 +22,26 @@ pub fn root_commits_by_refs<'a>(repo: &'a Repository) -> Result<Vec<Commit<'a>>,
 pub fn traverse_from_roots<'a>(
     repo: &'a Repository,
     commits: &[Commit<'a>],
-) -> Result<Vec<Commit<'a>>, Error> {
-    let mut visited = HashSet::new();
-    let mut queue = commits.iter().map(|x| x.id()).collect::<Vec<_>>();
+) -> Result<HashMap<Oid, Commit<'a>>, Error> {
+    let mut visited = HashMap::new();
+    let mut queue = commits.iter().map(|c| c.id()).collect::<Vec<_>>();
     while let Some(commit_id) = queue.pop() {
-        if !visited.insert(commit_id) {
+        if visited.contains_key(&commit_id) {
             continue;
         }
         let commit = repo.find_commit(commit_id)?;
         for parent_id in commit.parent_ids() {
             queue.push(parent_id);
         }
+        visited.insert(commit_id, commit);
     }
-    let mut commits = vec![];
-    for commit_id in visited {
-        let commit = repo.find_commit(commit_id)?;
-        commits.push(commit);
-    }
-    Ok(commits)
+    Ok(visited)
 }
 
 pub fn graph_from_refs(repo: &Repository) -> Result<Graph, Error> {
     let mut graph = Graph::new();
     let roots = root_commits_by_refs(&repo)?;
-    for commit in traverse_from_roots(&repo, &roots)? {
+    for commit in traverse_from_roots(&repo, &roots)?.values() {
         graph.add(&commit);
     }
     Ok(graph)
