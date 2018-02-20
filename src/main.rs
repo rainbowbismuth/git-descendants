84125e9001d0ab9dce12eb87937f7e597fa7c7a2
Emily Amanda Bellows <emily.a.bellows@gmail.com>
2018-02-20 17:07:35 -0500

    Refactored into subcommands

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,84 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate clap;
 extern crate git2;
 extern crate serde;
 extern crate serde_json;
 
+#[macro_use]
+extern crate clap;
+
+#[macro_use]
 extern crate failure;
 
 mod graph;
 mod calculate;
 
-use failure::Error;
-use clap::{App, Arg};
+use failure::{err_msg, Error};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use git2::{Commit, Repository};
+
+fn main() {
+    let repo_path = Arg::with_name("repo_path")
+        .help("Optionally specify the git repository to use")
+        .long("repo-path")
+        .short("p")
+        .takes_value(true);
+
+    let revision = Arg::with_name("REVISION")
+        .help("The revision you wish to know the children of")
+        .required(false)
+        .index(1);
+
+    let matches = App::new("git-descendants")
+        .version(crate_version!())
+        .author("Emily Amanda Bellows <emily.a.bellows@gmail.com>")
+        .about("Calculates an adjacency list of commits")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("graph")
+                .about("Calculate and write out the commit graph")
+                .arg(&repo_path),
+        )
+        .subcommand(
+            SubCommand::with_name("roots")
+                .about("Prints the roots of the graph")
+                .arg(&repo_path),
+        )
+        .subcommand(
+            SubCommand::with_name("children")
+                .about("Print the child commits of a given revision")
+                .arg(&repo_path)
+                .arg(&revision),
+        )
+        .get_matches();
+
+    match run_subcommand(matches) {
+        Ok(()) => {}
+        Err(err) => eprintln!("Error: {}", err),
+    }
+}
+
+fn run_subcommand(matches: ArgMatches) -> Result<(), Error> {
+    if let Some(matches) = matches.subcommand_matches("graph") {
+        let path = matches.value_of("repo_path").unwrap_or(".");
+        print_graph(path)
+    } else if let Some(matches) = matches.subcommand_matches("roots") {
+        let path = matches.value_of("repo_path").unwrap_or(".");
+        print_roots(path)
+    } else if let Some(matches) = matches.subcommand_matches("children") {
+        let path = matches.value_of("repo_path").unwrap_or(".");
+        let revision = matches
+            .value_of("REVISION")
+            .ok_or(err_msg("REVISION not specified"))?;
+        print_children(path, revision)
+    } else {
+        Err(format_err!(
+            "Unknown subcommand, {}",
+            matches.subcommand_name().unwrap()
+        ))
+    }
+}
 
 fn print_graph(path: &str) -> Result<(), Error> {
     let repo = Repository::open(path)?;
@@ -53,47 +118,4 @@ fn print_commit(commit: &Commit) {
         commit.id(),
         commit.summary().unwrap_or("<no message>").trim()
     )
-}
-
-fn main() {
-    let matches = App::new("git-descendants")
-        .version("0.1")
-        .author("Emily Amanda Bellows <emily.a.bellows@gmail.com>")
-        .about("Calculates an adjacency list of commits")
-        .arg(
-            Arg::with_name("repo_path")
-                .help("Optionally specify the git repository to use")
-                .long("repo-path")
-                .short("p")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("list_roots")
-                .long("list-roots")
-                .help("List the root commits of the graph"),
-        )
-        .arg(
-            Arg::with_name("REVISION")
-                .help("The revision you wish to know the children of")
-                .required(false)
-                .index(1),
-        )
-        .get_matches();
-
-    let path = matches.value_of("repo_path").unwrap_or(".");
-
-    check_err(if let Some(revision) = matches.value_of("REVISION") {
-        print_children(path, revision)
-    } else if matches.is_present("list_roots") {
-        print_roots(path)
-    } else {
-        print_graph(path)
-    });
-}
-
-fn check_err(res: Result<(), Error>) {
-    match res {
-        Ok(()) => {}
-        Err(err) => eprintln!("Error: {}", err),
-    }
 }
